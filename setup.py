1345aacc1d810b762414048c11f82774b75506dc
mcditooss <gerardofingurbe@gmail.com>
2024-02-14 12:03:59 +0100

    made qutip optional

--- setup.py
+++ setup.py
@@ -1,13 +1,11 @@
 from setuptools import setup
-from Cython.Build import cythonize
 
 setup(
     name='cumulant',
     version='0.0.1',
     install_requires=[
         'numpy',
-        'qutip',
         'scipy; python_version > "3.6"',
     ],
-    ext_modules=cythonize("integrals.pyx")
+    extras_require={"Full": ['qutip']},
 )
