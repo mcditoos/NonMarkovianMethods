6de2acc53fce951fd586875d5b47ea5e4def0bbe
mcditooss <gerardofingurbe@gmail.com>
2024-04-10 11:53:39 +0200

    problems with windows

--- nmm/__init__.py
+++ nmm/__init__.py
@@ -1,2 +1,2 @@
-from .utils import *
-from .cumulant import *
+from nmm.utils import *
+from nmm.cumulant import *
--- nmm/cumulant/__init__.py
+++ nmm/cumulant/__init__.py
@@ -1,2 +1,4 @@
+import sys, os
+sys.path.insert(0, os.path.abspath(".."))
 from nmm.cumulant.cumulant import csolve
 from nmm.cumulant.cum import bath_csolve
